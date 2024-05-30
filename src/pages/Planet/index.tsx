238d255488f0de84666e8047553a8e8a28c908fb
DavidKadaria <david.qada@gmail.com>
2024-05-31 03:58:01 +0400

    Modify Planet page

--- src/pages/Planet/index.tsx
+++ src/pages/Planet/index.tsx
@@ -1,7 +1,27 @@
-import type { Props } from './types';
+import { useEffect, useMemo } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { parsePlanetNames, parseParticularPlanetData } from '../../utils';
+import type { Planet as PlanetType } from '../../utils';
 
-function Planet({}: Props) {
-	return <div>Planet</div>;
+const planetNames = parsePlanetNames();
+
+function Planet() {
+	const { planetName } = useParams();
+	const navigate = useNavigate();
+
+	const planetData: PlanetType = useMemo(() => {
+		return parseParticularPlanetData(planetName!);
+	}, [planetName]);
+
+	useEffect(() => {
+		// Validate the planet name
+		if (!planetNames.find((name) => name.toLowerCase() === planetName)) {
+			// If the planet name is not valid, redirect to the default planet page
+			navigate('/mercury', { replace: true });
+		}
+	}, [navigate, planetName]);
+
+	return <div>{planetData.name}</div>;
 }
 
 export { Planet };
--- src/pages/Planet/types.ts
+++
@@ -1,3 +0,0 @@
-type Props = {};
-
-export type { Props };
