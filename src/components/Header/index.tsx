7fe24a5edabd669883aa2de29b406aedf12793f2
DavidKadaria <david.qada@gmail.com>
2024-05-31 01:13:07 +0400

    Modify Header component: implement static types for its props

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
+import type { Props } from './types';
 import './Header.css';
 
-function Header() {
+function Header({ data }: Props) {
 	return (
 		<header className='Header'>
 			<h1>Header</h1>
---
+++ src/components/Header/types.ts
@@ -0,0 +1,7 @@
+import type { PlanetNameAndImages } from '../../utils';
+
+type Props = {
+	data: PlanetNameAndImages[];
+};
+
+export type { Props };
