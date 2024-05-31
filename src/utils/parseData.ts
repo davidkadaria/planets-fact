7f7e0f166197d590cc1a68db26e1199a0df1df47
DavidKadaria <david.qada@gmail.com>
2024-05-31 04:08:11 +0400

    Remove unnecessary util method

--- src/utils/parseData.ts
+++ src/utils/parseData.ts
@@ -1,10 +1,6 @@
 import planetsData from '../data.json';
 import type { Planet } from '.';
 
-function parseData(): Planet[] {
-	return planetsData as Planet[];
-}
-
 function parsePlanetNames(): string[] {
 	return planetsData.map(({ name }) => name);
 }
@@ -13,4 +9,4 @@ function parseParticularPlanetData(planetName: string): Planet {
 	return planetsData.find(({ name }) => name.toLowerCase() === planetName) as Planet;
 }
 
-export { parseData, parsePlanetNames, parseParticularPlanetData };
+export { parsePlanetNames, parseParticularPlanetData };
